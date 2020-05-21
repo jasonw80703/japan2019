d197362ab84bbd5c4952b1620167a18610302ec2
Jason Wang <jason.wang@futureadvisor.com>
2020-05-20 17:48:40 -0700

    add easter egg

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,7 @@ import lowerCaseRemoveSpaces from './helpers/lowerCaseRemoveSpaces';
 import './Home.css';
 
 import homeText from './assets/texts/home.json';
+import lucy from './assets/images/home/lucy.jpg';
 
 /**
  * Map locations constant to Dropdown with an Item for each location
@@ -29,10 +30,12 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showLucy: false,
       showModal: false,
     };
 
     this.handleToggleModal = this.handleToggleModal.bind(this);
+    this.handleToggleLucy = this.handleToggleLucy.bind(this);
   }
 
   handleToggleModal() {
@@ -41,11 +44,18 @@ export default class Home extends Component {
     });
   }
 
+  handleToggleLucy() {
+    this.setState({
+      showLucy: !this.state.showLucy,
+    })
+  }
+
   render() {
-    const { showModal } = this.state;
+    const { showLucy, showModal } = this.state;
 
     return (
       <div>
+        <button type="button" className="lucy" onClick={this.handleToggleLucy}/>
         <div className="home">
           <h1 className="header" onClick={this.handleToggleModal}>{homeText.header}</h1>
           <LocationDropdown />
@@ -58,6 +68,9 @@ export default class Home extends Component {
             <Modal.Body>{homeText.modal.about}</Modal.Body>
             <Modal.Body>{homeText.modal.camera}</Modal.Body>
           </Modal>
+        )}
+        {showLucy && (
+          <img className="lucy-img" src={lucy} />
         )}
       </div>
     );
