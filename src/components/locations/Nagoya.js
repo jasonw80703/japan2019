e7164fcd94daa599c15a28a1514c7a45d2ffafc0
Jason <jasonw80702@gmail.com>
2020-05-25 09:33:14 -0700

    Add titles for all locations

--- src/components/locations/Nagoya.js
+++ src/components/locations/Nagoya.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LocationHeader from './LocationHeader';
-// import Page from '../blog/Page';
+import Page from '../blog/Page';
 // import Image from '../blog/Image';
 // import TextBlock from '../blog/TextBlock';
 // import TextHeader from '../blog/TextHeader';
@@ -12,7 +12,7 @@ export default function Nagoya() {
   return (
     <div>
       <LocationHeader dates={LOCATION['dates']} location={LOCATION['en']} nextLocation={LOCATION['next_location']} />
-      <h2 style={{ 'text-align': 'center' }}>Under Construction</h2>
+      <Page location={LOCATION['en']} locationJp={LOCATION['jp']}></Page>
     </div>
   )
 }
