402a74ab57de29daa796346d6e2e481e578d4fa0
Jason <jasonw80702@gmail.com>
2020-05-29 11:02:06 -0700

    use carousel in image slide

--- src/components/blog/ImageSlide.js
+++ src/components/blog/ImageSlide.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
 import ImageModal from 'components/blog/ImageModal';
 import './ImageSlide.css';
 
@@ -20,33 +16,7 @@ export default class ImageSlide extends Component {
       showImageModal: false,
     };
 
-    this.prevImage = this.prevImage.bind(this);
-    this.nextImage = this.nextImage.bind(this);
     this.handleToggleImageModal = this.handleToggleImageModal.bind(this);
-  }
-
-  prevImage() {
-    const { images } = this.props;
-    const { currentIndex } = this.state;
-
-    const newIndex = currentIndex === 0 ? images.length - 1: currentIndex - 1;
-
-    this.setState({
-      currentIndex: newIndex,
-      currentImage: images[newIndex],
-    });
-  }
-
-  nextImage() {
-    const { images } = this.props;
-    const { currentIndex } = this.state;
-
-    const newIndex = (currentIndex + 1) % images.length;
-
-    this.setState({
-      currentIndex: newIndex,
-      currentImage: images[newIndex],
-    });
   }
 
   handleToggleImageModal() {
@@ -58,20 +28,29 @@ export default class ImageSlide extends Component {
   }
 
   render() {
-    const { location } = this.props;
+    const { images, location } = this.props;
     const { currentImage, showImageModal } = this.state;
 
     return (
-      <div id='slide-container'>
-        <Container>
-          <Row>
-            <Col align="center" className="slide-btn" onClick={this.prevImage}><Button variant="dark">{'<'}</Button></Col>
-            <Col lg={8}>
-              <Image src={currentImage} className="slide-pic" onClick={this.handleToggleImageModal}/>
-            </Col>
-            <Col align="center" className="slide-btn" onClick={this.nextImage}><Button variant="dark">{'>'}</Button></Col>
-          </Row>
-        </Container>
+      <div>
+        <Carousel
+          className='carousel-container'
+          nextIcon={<span className="fa fa-angle-right fa-3x arrow-icon" />}
+          prevIcon={<span className="fa fa-angle-left fa-3x arrow-icon" />}
+        >
+          {
+            images.map((img, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  src={img}
+                  alt={'wow hi there'}
+                  className='carousel-img'
+                  onClick={this.handleToggleImageModal}
+                />
+              </Carousel.Item>
+            ))
+          }
+        </Carousel>
         <ImageModal
           handleCloseModal={this.handleToggleImageModal}
           title={location}
@@ -79,7 +58,7 @@ export default class ImageSlide extends Component {
           showImageModal={showImageModal}
         />
       </div>
-    )
+    );
   }
 }
 
