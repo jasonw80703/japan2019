c23ce0b2d680f9af3da1bdbda24d77d7ad4558cd
Jason <jasonw80702@gmail.com>
2020-06-06 09:37:48 -0700

    Updates

---
+++ precache-manifest.2e5b681e9e520bea2a41e8e2d13f5971.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121b48259664740b86f0b0c1d983b756",
+    "url": "/japan2019/index.html"
+  },
+  {
+    "revision": "bfd445f96d259fb383f2",
+    "url": "/japan2019/static/css/2.6ccefa39.chunk.css"
+  },
+  {
+    "revision": "b3d3ca24bfe77e891f04",
+    "url": "/japan2019/static/css/main.be658e96.chunk.css"
+  },
+  {
+    "revision": "bfd445f96d259fb383f2",
+    "url": "/japan2019/static/js/2.230ea114.chunk.js"
+  },
+  {
+    "revision": "865f46d05fc3c49c93898fd56e06d864",
+    "url": "/japan2019/static/js/2.230ea114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d3ca24bfe77e891f04",
+    "url": "/japan2019/static/js/main.8d188907.chunk.js"
+  },
+  {
+    "revision": "f3654dd03fca1aae2e05",
+    "url": "/japan2019/static/js/runtime-main.00193138.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/japan2019/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/japan2019/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/japan2019/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/japan2019/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/japan2019/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/japan2019/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/japan2019/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/japan2019/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/japan2019/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/japan2019/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/japan2019/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/japan2019/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/japan2019/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/japan2019/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/japan2019/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "eb2546b952e7bfe93a695f8f3ff520d3",
+    "url": "/japan2019/static/media/hakodate.eb2546b9.jpg"
+  },
+  {
+    "revision": "9820c6f56abd691f4ecc32bedab4170b",
+    "url": "/japan2019/static/media/sanfrancisco.9820c6f5.jpg"
+  },
+  {
+    "revision": "b76fcd81b61cdfb21891078cba0f4b9c",
+    "url": "/japan2019/static/media/sapporo.b76fcd81.jpg"
+  }
+]);
