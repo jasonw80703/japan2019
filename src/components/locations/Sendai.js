404de518ddae9a614cf5bdde6b927f9a234bbf2d
Jason Wang <jasonw80702@gmail.com>
2020-12-04 22:12:35 -0500

    sendai * back from hiatus * let's goooooo

--- src/components/locations/Sendai.js
+++ src/components/locations/Sendai.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { getS3ObjectImagePath } from 'helpers/getS3ObjectPath';
 import LocationHeader from 'components/locations/LocationHeader';
 import Page from 'components/blog/Page';
-// import Image from 'components/blog/Image';
-// import TextBlock from 'components/blog/TextBlock';
+import Image from 'components/blog/Image';
+import TextBlock from 'components/blog/TextBlock';
 // import TextHeader from 'components/blog/TextHeader';
 import { LOCATIONS } from 'constants/locations';
+
+// Map
+import map from 'assets/map/sendai.jpg';
+import mapModalDetails from 'assets/map/sendai.json';
+
+// Texts
+import sendaiText from 'assets/texts/sendai.json';
 
 const LOCATION = LOCATIONS.SENDAI;
 
@@ -13,8 +21,14 @@ export default function Sendai() {
     <div>
       <LocationHeader
         location={LOCATION}
+        map={map}
+        mapModalDetails={mapModalDetails}
       />
-      <Page location={LOCATION}></Page>
+      <Page location={LOCATION}>
+        <Image source={getS3ObjectImagePath('sendai/sendai')} location={LOCATION['en']} />
+        <TextBlock text={sendaiText.intro} />
+        <Image source={getS3ObjectImagePath('sendai/aer')} location={LOCATION['en']} />
+      </Page>
     </div>
   )
 }
