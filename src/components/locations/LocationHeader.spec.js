34178eb4eb18fd5e87148092a15d9b3b441a91eb
Jason Wang <jasonw80702@gmail.com>
2021-02-03 12:21:25 -0800

    wow! Let's use 16.8's useState state hook

--- src/components/locations/LocationHeader.spec.js
+++ src/components/locations/LocationHeader.spec.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import LocationHeader from 'components/locations/LocationHeader';
 import { LOCATIONS } from 'constants/locations';
 import map from 'assets/map/sanfrancisco.jpg';
 import mapModalDetails from 'assets/map/sanfrancisco.json';
 
 describe('<LocationHeader />', () => {
-  let wrapper;
 
   const defaultProps = {
     location: LOCATIONS.SAN_FRANCISCO,
@@ -20,13 +18,5 @@ describe('<LocationHeader />', () => {
       .create(<LocationHeader {...defaultProps} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
-  });
-
-  describe('when handleToggleMapModal() is called', () => {
-    wrapper = shallow(<LocationHeader {...defaultProps} />);
-    wrapper.instance().handleToggleMapModal();
-    expect(wrapper.state('showMapModal')).toEqual(true);
-    wrapper.instance().handleToggleMapModal();
-    expect(wrapper.state('showMapModal')).toEqual(false);
   });
 });
